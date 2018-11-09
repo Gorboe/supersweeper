289c49c727a9a58c7f5bb460f0269909f1c26053
Gorboe <31539445+Gorboe@users.noreply.github.com>
2018-11-09 14:31:32 +0100

    added some buttons, connected to the titlecontroller

--- src/controllers/TitleController.java
+++ src/controllers/TitleController.java
@@ -1,4 +1,16 @@
 package controllers;
 
 public class TitleController {
+
+    public void handleStartClassic(){
+        System.out.println("starting classic game");
+    }
+
+    public void handleStartTrio(){
+        System.out.println("starting trio game");
+    }
+
+    public void handleStartHex(){
+        System.out.println("starting hex game");
+    }
 }
--- src/core/Main.java
+++ src/core/Main.java
@@ -4,7 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -12,11 +15,23 @@ public class Main extends Application {
         launch(args);
     }
 
+    private static Stage primaryStage;
+    public static final int SCREEN_WIDTH = 600;
+    public static final int SCREEN_HEIGHT = 600;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../views/titleview.fxml"));
+        Main.primaryStage = primaryStage;
+        changeScene("titleview.fxml");
+    }
+
+    public static void changeScene(String sceneName) throws IOException {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/views/" + sceneName));
+        GridPane root = loader.load();
+        Scene scene = new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT);
+
         primaryStage.setTitle("SuperSweeper");
-        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
