f4d81237cc6cd50909eecf59e900246b708d2dfe
Gorboe <31539445+Gorboe@users.noreply.github.com>
2018-11-09 14:09:50 +0100

    added canvas to fxml and linked to controller
    +scene size

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML Canvas canvas;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("initializing stuff in controller");
+    }
 }
